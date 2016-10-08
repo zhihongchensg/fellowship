1f916b2f7d38cfd257755641438f0f6a2b68c122
zhihongchensg <zhihongchensg@gmail.com>
2016-10-09 01:58:04 +0800

    zhihong answers lotr

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -24,6 +24,34 @@ var body = document.querySelector('body');
 
 
 function makeMiddleEarth() {
+  var midEarth = document.createElement('section')
+  midEarth.setAttribute('id', 'middle-earth')
+  var shireLand = document.createElement('article')
+  var rivendell = document.createElement('article')
+  var mordor = document.createElement('article')
+  var shireLandH1 = document.createElement('h1')
+  var rivendellH1 = document.createElement('h1')
+  var mordorH1 = document.createElement('h1')
+  var shireLandtxt = document.createTextNode('The Shire')
+  var rivendelltxt = document.createTextNode('Rivendell')
+  var mordortxt = document.createTextNode('Mordor')
+
+  shireLandH1.appendChild(shireLandtxt)
+  shireLand.appendChild(shireLandH1)
+  midEarth.appendChild(shireLand)
+
+  rivendellH1.appendChild(rivendelltxt)
+  rivendell.appendChild(rivendellH1)
+  midEarth.appendChild(rivendell)
+
+  mordorH1.appendChild(mordortxt)
+  mordor.appendChild(mordorH1)
+  midEarth.appendChild(mordor)
+
+  body.appendChild(midEarth)
+
+
+
   // create a section tag with an id of middle-earth
   // add each land as an article tag
   // inside each article tag include an h1 with the name of the land
@@ -36,45 +64,84 @@ makeMiddleEarth();
 // Part 2
 
 function makeHobbits() {
+
+  var list = document.createElement('ul')
+  for (var x = 0; x < hobbits.length; x++) {
+    var listItem = document.createElement('li')
+    listItem.textContent = hobbits[x]
+    listItem.className = 'hobbit'
+    list.appendChild(listItem)
+  }
+var temp = document.querySelectorAll('article')
+temp[0].appendChild(list)
   // display an unordered list of hobbits in the shire (which is the second article tag on the page)
   // give each hobbit a class of hobbit
 }
 
-
+makeHobbits();
 // Part 3
 
+var ShireBody = document.querySelectorAll('article')[0]
+var Frodo = ShireBody.querySelectorAll('li')[0]
+
 function keepItSecretKeepItSafe() {
+  var theRing = document.createElement('div')
+  theRing.setAttribute('id', 'the-ring')
+  theRing.setAttribute('class', 'magic-imbued-jewelry')
+  theRing.addEventListener('click', nazgulScreech)
+  Frodo.appendChild(theRing)
   // create a div with an id of 'the-ring'
   // give the div a class of 'magic-imbued-jewelry'
   // add an event listener so that when a user clicks on the ring, the nazgulScreech function (provided) is invoked
   // add the ring as a child of Frodo
 }
 
+keepItSecretKeepItSafe();
 
 // Part 4
+var rivendell = body.querySelectorAll('article')[1]
 
-
-function makeBuddies() {
+function makeBuddies () {
+  var aside = document.createElement('aside')
+  var asideList = document.createElement('ul')
+  for (var x = 0; x < buddies.length; x++) {
+    var asideListItem = document.createElement('li')
+    asideListItem.textContent = buddies[x]
+    asideList.appendChild(asideListItem)
+  }
+  aside.appendChild(asideList)
+  rivendell.appendChild(aside)
   // create an aside tag
   // attach an unordered list of the 'buddies' in the aside
   // insert your aside as a child element of rivendell
 }
+
+makeBuddies()
 
 
 // Part 5
 
 
 function beautifulStranger() {
+
+rivendell.querySelectorAll('li')[3].textContent = 'Aragorn'
+
   // change the 'Strider' textnode to 'Aragorn'
 }
 
+beautifulStranger()
 
 // Part 6
 
 function leaveTheShire() {
+  temp = ShireBody.querySelector('ul')
+  while (temp.hasChildNodes()) {
+    rivendell.appendChild(temp.removeChild(temp.firstChild))
+  }
   // assemble the hobbits and move them to Rivendell
 }
 
+leaveTheShire()
 
 // Part 7
 
@@ -83,6 +150,11 @@ function forgeTheFellowShip() {
   // create a new div called 'the-fellowship' within rivendell
   // add each hobbit and buddy one at a time to 'the-fellowship'
   // after each character is added make an alert that they have joined your party
+
+  // var the-fellowship = document.createElement('div')
+
+
+
 }
 
 
